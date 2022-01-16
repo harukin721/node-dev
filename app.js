65e044aae2a64a232c303f7f66bcc0844c8d0ffb
上島春輝 <h.ueji.43@gmail.com>
2022-01-16 16:41:36 +0900

    静的コンテンツの配信

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 const express = require("express");
 const app = express();
 
+// Express setting
 app.set("view engine", "ejs");
 
+// Static resource rooting
+// __dirname: 現在のフォルダから。
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+// Dynamic resource root
 app.use("/", require("./routes/index.js"));
 
+// Execute web application
 app.listen(PORT, () => {
     console.log(`Application listening at :${PORT}`);
 });
