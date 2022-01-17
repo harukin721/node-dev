a789b6a51ac229d000261e0f28f88798842deb18
上島春輝 <h.ueji.43@gmail.com>
2022-01-17 20:19:28 +0900

    mysql接続

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
-const accesslogger = require("./lib/log/accesslogger");
-const applicationlogger = require("./lib/log/applicationlogger");
+const accesslogger = require("./lib/log/accesslogger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
 
-// Express setting
+// Express settings
 app.set("view engine", "ejs");
-app.disable("X-powered-by");
+app.disable("x-powered-by");
 
-// Static resource rooting
+// Static resource rooting.
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
-// __dirname: 現在のフォルダから。
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
-// Set access log
+// Set access log.
 app.use(accesslogger());
 
-// Dynamic resource root
+// Dynamic resource rooting.
 app.use("/", require("./routes/index.js"));
+app.use("/test", async (req, res, next) => {
+  const { promisify } = require("util");
+  const path = require("path");
+  const { sql } = require("@garafu/mysql-fileloader")({ root: path.join(__dirname, "./lib/database/sql") });
+  const config = require("./config/mysql.config.js");
+  const mysql = require("mysql");
+  const con = mysql.createConnection({
+    host: config.HOST,
+    port: config.PORT,
+    user: config.USERNAME,
+    password: config.PASSWORD,
+    database: config.DATABASE
+  });
+  const client = {
+    connect: promisify(con.connect).bind(con),
+    query: promisify(con.query).bind(con),
+    end: promisify(con.end).bind(con)
+  };
+  var data;
 
-// Set application rooting
+  try {
+    await client.connect();
+    data = await client.query(await sql("SELECT_SHOP_BASIC_BY_ID"));
+    console.log(data);
+  } catch (err) {
+    next(err);
+  } finally {
+    await client.end();
+  }
+
+  res.end("OK");
+});
+
+// Set application log.
 app.use(applicationlogger());
 
-// Execute web application
+// Execute web application.
 app.listen(PORT, () => {
   logger.application.info(`Application listening at :${PORT}`);
 });
