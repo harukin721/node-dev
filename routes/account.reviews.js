16470cc5875a1c2f42fc351582c65060457a485c
上島春輝 <h.ueji.43@gmail.com>
2022-01-25 23:39:44 +0900

    確認画面の実装完了

--- routes/account.reviews.js
+++ routes/account.reviews.js
@@ -1,5 +1,34 @@
 const router = require("express").Router();
 const { MySQLClient, sql } = require("../lib/database/client.js");
+const moment = require("moment");
+const DATE_FORMAT = "YYYY/MM/DD";
+
+var validateReviewData = function (req) {
+  var body = req.body;
+  var isValid = true, error = {};
+
+  if (body.visit && !moment(body.visit, DATE_FORMAT).isValid()) {
+    isValid = false;
+    error.visit = "訪問日の日付文字列が不正です。";
+  }
+
+  if (isValid) {
+    return undefined;
+  }
+  return error;
+};
+
+var createReviewData = function (req) {
+  var body = req.body, date;
+
+  return {
+    shopId: req.params.shopId,
+    score: parseFloat(body.score),
+    visit: (date = moment(body.visit, DATE_FORMAT)) && date.isValid() ? date.toDate() : null,
+    post: new Date(),
+    description: body.description
+  };
+};
 
 router.get("/regist/:shopId(\\d+)", async (req, res, next) => {
   var shopId = req.params.shopId;
@@ -17,6 +46,25 @@ router.get("/regist/:shopId(\\d+)", async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+router.post("/regist/:shopId(\\d+)", async (req, res, next) => {
+  var review = createReviewData(req);
+  var { shopId, shopName } = req.body;
+  res.render("./account/reviews/regist-form.ejs", { shopId, shopName, review });
+});
+
+router.post("/regist/confirm", (req, res) => {
+  var error = validateReviewData(req);
+  var review = createReviewData(req);
+  var { shopId, shopName } = req.body;
+
+  if (error) {
+    res.render("./account/reviews/regist-form.ejs", { error, shopId, shopName, review });
+    return;
+  }
+
+  res.render("./account/reviews/regist-confirm.ejs", { shopId, shopName, review });
 });
 
 module.exports = router;
